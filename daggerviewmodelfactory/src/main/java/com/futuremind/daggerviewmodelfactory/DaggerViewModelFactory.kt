8723a569706f31cc76fe03022cdc7b512b82064c
Michał Klimczak <m.klimczak@futuremind.com>
2017-10-11 14:39:27 +0200

    init

---
+++ daggerviewmodelfactory/src/main/java/com/futuremind/daggerviewmodelfactory/DaggerViewModelFactory.kt
@@ -0,0 +1,31 @@
+package com.futuremind.daggerviewmodelfactory
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import javax.inject.Provider
+
+@javax.inject.Singleton
+class DaggerViewModelFactory
+@javax.inject.Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("unknown model class " + modelClass)
+        }
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+    }
+}
---
+++ daggerviewmodelfactory/src/main/java/com/futuremind/daggerviewmodelfactory/ViewModelKey.kt
@@ -0,0 +1,9 @@
+package com.futuremind.daggerviewmodelfactory
+
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(RUNTIME)
+@dagger.MapKey
+annotation class ViewModelKey(val value: KClass<out android.arch.lifecycle.ViewModel>)
